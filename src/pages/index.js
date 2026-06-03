3089079cbee171294116dcb2a86513d6f7385a4a
Kaleshe <kaleshe@neuralalpha.com>
2026-06-03 20:59:26 +0100

    update meta on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,21 +22,21 @@ export const Head = () => {
   return (
     <>
       <Helmet>
-        <title>Kaleshe | Web designer & developer</title>
+        <title>Kaleshe | Helping small businesses grow with user-friendly websites</title>
         <meta
           name="description"
-          content="A frontend developer with over 3 years of professional experience in web development, UI design, and JavaScript frameworks such as React."
+          content="Web designer creating modern, user-friendly websites that help small businesses grow online."
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="twitter:site" contact="@stubbornneko"></meta>
         <meta name="twitter:creator" content="@stubbornneko"></meta>
         <meta
           name="twitter:title"
-          content="Kaleshe | Web designer & developer"
+          content="Kaleshe | Helping small businesses grow with user-friendly websites"
         ></meta>
         <meta
           name="twitter:description"
-          content="A frontend developer with over 3 years of professional experience in web development, UI design, and JavaScript frameworks such as React."
+          content="Web designer creating modern, user-friendly websites that help small businesses grow online."
         ></meta>
         <meta name="twitter:image" content="../images/og_image.png"></meta>
         ​
@@ -44,12 +44,12 @@ export const Head = () => {
         <meta property="og:site_name" content="Kaleshe" />
         <meta
           property="og:title"
-          content="Kaleshe | Web designer & developer"
+          content="Kaleshe | Helping small businesses grow with user-friendly websites"
         ></meta>
         <meta property="og:url" content="https://www.kaleshe.co.uk"></meta>
         <meta
           property="og:description"
-          content="A frontend developer with over 3 years of professional experience in web development, UI design, and JavaScript frameworks such as React."
+          content="Web designer creating modern, user-friendly websites that help small businesses grow online."
         ></meta>
         <meta property="og:image" content="../images/open_graph.png"></meta>
         <meta property="og:image:alt" content="Kaleshe"></meta>
